6f949852e30e2418e398018f998892d8cafe9b6f
Drew <drew.seminara@gartrellgroup.com>
2017-03-23 15:00:23 -0700

    add default dimensions/spacing on pdf

--- js/print.js
+++ js/print.js
@@ -6,22 +6,16 @@ function PrintControl(options) {
 
 PrintControl.prototype.onAdd = function(map) {
     this._map = map;
-
     this._container = document.createElement('div');
-    this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+    this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group print';
 
     this._printBtn = document.createElement('button');
     this._printBtn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-print';
 
-    this._printBtn.setAttribute('data-toggle', 'modal');
-    this._printBtn.setAttribute('data-target', '#map-print-modal') // #map-print-modal is the DOM  ID
-
-    this._printBtn.addEventListener('mousedown', this.initializeCropper.bind(this));
-
-    // export-map is the primary button within the map-print-modal
-    document.querySelector('#export-map').addEventListener('mousedown', this.onPrintDown.bind(this));
-
     this._container.appendChild(this._printBtn);
+    this._container.style.display = 'none';
+
+    this.onReady(map);
     return this._container;
 }
 
@@ -31,20 +25,37 @@ PrintControl.prototype.onRemove = function() {
     return this;
 }
 
-PrintControl.prototype.initializeCropper = function (e) {
-    var _this = this;
+PrintControl.prototype.onReady = function(map) {
+    var that = this;
 
+    var layersLoaded = function() {
+        if (map.loaded()) {
+            that._printBtn.setAttribute('data-toggle', 'modal');
+            that._printBtn.setAttribute('data-target', '#map-print-modal') // #map-print-modal is the DOM  ID
+            that._printBtn.addEventListener('mousedown', that.initializeCropper.bind(that));
+
+            // export-map is the primary button within the map-print-modal
+            document.querySelector('#export-map').addEventListener('mousedown', that.onPrintDown.bind(that));
+            that._container.style.display = 'block';
+            map.off('render', layersLoaded)
+        }
+    }
+
+    map.on('render', layersLoaded);
+}
+
+
+PrintControl.prototype.initializeCropper = function (e) {
     var exportView = $('#export-view');
     exportView.attr('src', map.getCanvas().toDataURL());
     exportView.css('max-width', '100%')
 
-    _this.cropper = exportView.cropper({
-        aspectRatio: 1.5/1,
+    this.cropper = exportView.cropper({
+        aspectRatio: 1.3/1,
         zoomable: false,
         zoomOnWheel: false,
         minContainerHeight: 300,
         minContainerWidth: 568,
-
     })
 }
 
@@ -62,41 +73,39 @@ PrintControl.prototype.onPrintDown = function (e) {
 
 PrintControl.prototype.printCanvas = function(type, size, zoom, center, bearing) {
     var _this = this;
+    var dimensions = (size === 'default') ? [816, 1056] : [1056, 1632];
 
-    //based on Print Maps - https://github.com/mpetroff/print-maps
-    var actualPixelRatio = window.devicePixelRatio;
-    Object.defineProperty(window, 'devicePixelRatio', {
-        get: function() {return dpi / 96}
-    });
-
-    if (type == 'png') {
+    if (type === 'png') {
         var mapCanvas = map.getCanvas();
         var tmpCanvas = document.createElement('canvas');
-        tmpCanvas.width = _this.toPixels(width);
-        tmpCanvas.height = _this.toPixels(height);
+        tmpCanvas.width = _this.toPixels(dimensions[1]);
+        tmpCanvas.height = _this.toPixels(dimensions[0]);
 
         var ctx = tmpCanvas.getContext('2d');
-        ctx.drawImage(mapCanvas,0,0,mapCanvas.width,mapCanvas.height,0,0,_this.toPixels(width),_this.toPixels(height));
+        ctx.drawImage(mapCanvas,0,0,mapCanvas.width,mapCanvas.height,0,0,tmpCanvas.width,tmpCanvas.height);
 
         tmpCanvas.toBlob(function(blob) {
             saveAs(blob, 'map.png');
         });
     } else {
         var pdf = new jsPDF({
-            orientation: width > height ? 'l' : 'p',
-            unit: unit,
-            format: [width, height],
-            compress: true
+            orientation: 'landscape',
+            unit: 'px',
+            format: dimensions
         });
 
-        pdf.addImage(map.getCanvas().toDataURL('image/png'),
-            'png', 0, 0, width, height, null, 'FAST');
-        pdf.save('map.pdf');
-    }
+        if (size === 'default') {
+            pdf.addImage(map.getCanvas().toDataURL('image/png'),
+                'png', 5, 5, 1046, 705);
+            pdf.save('map.pdf');
+        } else {
+            pdf.addImage(map.getCanvas().toDataURL('image/png'),
+                'png', 5, 5, 1435, 965);
+            pdf.save('map.pdf');
+        }
 
-    Object.defineProperty(window, 'devicePixelRatio', {
-        get: function() {return actualPixelRatio}
-    });
+
+    }
 }
 
 PrintControl.prototype.toPixels = function(val) {
